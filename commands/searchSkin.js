23227967477512c9ac8b4d74ecd3e6a065a8a89f
Loxt_Nixo <128501463+loxt-nixo@users.noreply.github.com>
2025-02-21 22:48:48 +0100

    Add files via upload

--- commands/searchSkin.js
+++ commands/searchSkin.js
@@ -1,4 +1,8 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
+
+function addSpaceBeforeCapitalized(str) {
+    return str.replace(/([a-z])([A-Z])/g, '$1 $2');
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -32,13 +36,70 @@ module.exports = {
 
         const skinTier = client.skinsTier.find(s => s["uuid"] === foundSkin["contentTierUuid"]);
 
+        const skinPrice = await fetch("https://val-skin-price.vercel.app/beta/skins/" + skinUUID);
+        const skinPriceData = await skinPrice.json();
+
         const Embed = new EmbedBuilder()
         .setColor(`#${skinTier["highlightColor"].slice(0, -2)}` || "DarkerGrey")
         .setTitle(foundSkin["displayName"]["en-US"])
         .setThumbnail(skinTier["displayIcon"])
         .setImage(foundSkin["levels"][0]["displayIcon"] || foundSkin["displayIcon"])
-        .setDescription(`Price: ${!foundSkin["displayName"]["en-US"].includes('Knife') ? skinTier["price"] || 0 : "1750 - 5950"} ${client.config.Emojis["ValoPoints"]}`)
+        .setDescription(`Price: ${skinPriceData["price"] || skinTier["price"] || 'Error!'} ${client.config.Emojis["ValoPoints"]}`)
 
-        await interaction.reply({ embeds: [Embed] });
+        const SkinChromas = new ActionRowBuilder()
+        const SkinLevels = new ActionRowBuilder()
+        const Previews = new ActionRowBuilder()
+        
+        let i = 0;
+
+        for (const chroma of foundSkin["chromas"]) {
+            const chromaEmojiData = client.swatch.find(s => s.uuid == chroma["uuid"]);
+
+            const ChromaButton = new ButtonBuilder()
+            .setCustomId(`skin-chroma_${skinUUID}_${i}`)
+            .setStyle(ButtonStyle.Primary)
+            .setLabel(`${i == 0 ? "Base Variant" : `Variant ${i}`}`)
+            .setDisabled(i == 0 ? true : false)
+            
+            if (chromaEmojiData) {
+                ChromaButton.setEmoji({ id: chromaEmojiData["emojiId"] });
+            }
+
+            SkinChromas.addComponents(
+                ChromaButton
+            )
+            i++;
+        }
+
+        i = 0;
+
+        for (const level of foundSkin["levels"]) {
+            const LevelItem = level["levelItem"] ? addSpaceBeforeCapitalized(level["levelItem"].split("::")[1]) : null;
+
+            SkinLevels.addComponents(
+                new ButtonBuilder()
+                .setCustomId(`skin-level_${skinUUID}_${i}`)
+                .setStyle(ButtonStyle.Primary)
+                .setLabel(`${i == 0 ? 'Base Level' : `Level ${i} - ${LevelItem}`}`)
+                .setDisabled(i == 0 ? true : false)
+            )
+            i++;
+        }
+
+        Previews.addComponents(
+            new ButtonBuilder()
+            .setCustomId(`skin-preview_chromas_${skinUUID}_0`)
+            .setStyle(ButtonStyle.Primary)
+            .setDisabled(foundSkin["levels"][0]["streamedVideo"] ? false : true)
+            .setLabel(`Variant Preview`),
+
+           new ButtonBuilder()
+           .setCustomId(`skin-preview_levels_${skinUUID}_0`)
+           .setDisabled(foundSkin["levels"][0]["streamedVideo"] ? false : true)
+           .setStyle(ButtonStyle.Primary)
+           .setLabel(`Level Preview`)
+        )
+
+        await interaction.reply({ embeds: [Embed], components: [SkinChromas, SkinLevels, Previews] });
     }
 }
